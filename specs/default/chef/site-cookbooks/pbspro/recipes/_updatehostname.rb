f7ab8cce5f3cb7912a9474795cd1a848180b6fa6
Michael Weiser <michael.weiser@eviden.com>
2025-05-13 15:31:37 +0200

    pbs: Switch node hostnames to explicit renaming
    ... similar to how slurm does it

--- specs/default/chef/site-cookbooks/pbspro/recipes/_updatehostname.rb
+++ specs/default/chef/site-cookbooks/pbspro/recipes/_updatehostname.rb
@@ -4,6 +4,20 @@
 # Recipe:: _updatehostname
 #
 
+use_nodename_as_hostname = node[:pbspro][:use_nodename_as_hostname]
+if use_nodename_as_hostname
+  node_hostname = node[:cyclecloud][:node][:name]
+  node_prefix = node[:pbspro][:node_prefix]
+  if node_prefix
+    node_hostname = "#{node_prefix}#{node_hostname}"
+  end
+
+  # from https://github.com/chef-boneyard/chef_hostname/blob/c9838b625916d5e2ec1459b0eddc6c6405d87f37/resources/hostname.rb#L93C1-L96C12
+  execute "hostnamectl set-hostname #{node_hostname}" do
+    not_if { shell_out!("hostnamectl status", { :returns => [0, 1] }).stdout =~ /Static hostname:\s+#{node_hostname}/ }
+  end
+end
+
 bash "update hostname via jetpack" do
     code <<-EOF
     #{node[:cyclecloud][:home]}/system/embedded/bin/python -c "import jetpack.converge as jc; jc._send_installation_status('warning')"
